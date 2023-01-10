2c0bf9fed7f1202aee7173a787ab93f88d405cdd
rinkal-yadav <rinkal95@gamil.com>
2023-01-10 10:32:02 +0530

    comment

--- constants/data.js
+++ constants/data.js
@@ -109,7 +109,8 @@ export const products = [
     },
     { 
         id: 'product7',
-        url: 'https://rukminim1.flixcart.com/image/150/150/kcgk1ow0/headphone/n/u/a/235v2-fast-charging-boat-original-imaftk6us4af7bca.jpeg?q=70',
+        url:'https://rukminim1.flixcart.com/image/150/150/xif0q/shirt/d/t/h/xl-st10-vebnor-original-imagkngtaq48krmv.jpeg?q=70',
+        // url: 'https://rukminim1.flixcart.com/image/150/150/kcgk1ow0/headphone/n/u/a/235v2-fast-charging-boat-original-imaftk6us4af7bca.jpeg?q=70',
         detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kcgk1ow0/headphone/n/u/a/235v2-fast-charging-boat-original-imaftk6us4af7bca.jpeg?q=70', 
         title: {
             shortTitle: 'Headphones',
@@ -124,5 +125,23 @@ export const products = [
         description: 'Let music brighten up your mood anytime, anywhere with the boAt 235v2 Fast Charging Bluetooth Headset. This Bluetooth headset features a Call Vibration Alert, a Fast Charging Technology, and Easy Access Controls to listen to and manage your favorite music with ease.',
         discount: 'Minimum 50% Off', 
         tagline: 'Grab Now!' 
-    }
-];
+    
+        }
+]
+
+// export const Shirts = [
+//     {
+//         id: '',
+//         url: '', 
+//         detailUrl: '',
+//         title: {
+//             shortTitle: '',
+//             longTitle: ''
+//         }, 
+//         price: {
+//             mrp: '',
+//             cost: '',
+//             discount:'' 
+//         },
+//     }
+// ]
